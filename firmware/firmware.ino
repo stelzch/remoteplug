c2b4f55697a251ce8c64467a42d5b9f0847e919f
Christoph Stelz <mail@ch-st.de>
2020-12-14 19:29:15 +0100

    Expose RCOutlet over HTTPS-API
    There are now two endpoints: `/switch/a/off` and `/switch/a/on` that can
    be POSTed to turn on the power outlet via 433 MHz. The HTTP server
    listens on port 443 with a self-signed certificate.
    
    The certificate can be generated with openssl:
    
    `openssl req -x509 -newkey rsa:1024 -sha256 -keyout key.txt -out
    cert.txt -days 3650 -nodes`
    
    The content of key.txt must be put into the string SERVER_KEY and
    cert.txt into SERVER_CERT.
    
    For testing, curl can be used:
    `curl -X POST --insecure "https://192.168.0.113/switch/a/on"`
    
    Furthermore, files inside the data directory are now served under the
    /static URL.

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,17 +1,72 @@
 #include "config.h"
 #include "RCOutlet.h"
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServerSecure.h>
+#include <ESP8266WebServer.h>
+#include <LittleFS.h>
 
 RCOutlet rco(13);
-
+BearSSL::ESP8266WebServerSecure server(443);
+//ESP8266WebServer server(80);
 
 void setup() {
+  Serial.begin(115200);
+  if(!LittleFS.begin()) {
+    Serial.println("[ERR] Could not initialize file system");
+  }
+
   
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PSK);
+  Serial.println("");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.printf("\nConnected, IP: ");
+  Serial.println(WiFi.localIP());
+
+
+  server.getServer().setRSACert(
+    new BearSSL::X509List(SERVER_CERT),
+    new BearSSL::PrivateKey(SERVER_KEY)
+  );
+
+  server.on("/", []() {
+    server.send(200, "text/plain", "Hello World!");
+  });
+
+  server.on("/switch/a/on", []() {
+    if (server.method() != HTTP_POST) {
+      // Method Not Allowed
+      server.send(405, "text/plain",
+        ESP8266WebServerSecure::responseCodeToString(405));
+    }
+
+    rco.switchA(true);
+    server.send(200, "text/plain", "ON");
+  });
+
+  server.on("/switch/a/off", []() {
+    if (server.method() != HTTP_POST) {
+      // Method Not Allowed
+      server.send(405, "text/plain",
+        ESP8266WebServerSecure::responseCodeToString(405));
+    }
+
+    rco.switchA(false);
+    server.send(200, "text/plain", "OFF");
+  });
+
+  server.serveStatic("/static", LittleFS, "/", NULL);
+
+  server.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  rco.switchA(true);
-  delay(2000);
-  rco.switchA(false);
-  delay(2000);
+  server.handleClient();
 }
